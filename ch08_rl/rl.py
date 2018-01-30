e3bc5812668b431f6398fb2ac156148f1ba19fa9
Udo Held <github@udoheld.com>
2018-01-30 22:25:38 +1100

    Update rl.py

--- ch08_rl/rl.py
+++ ch08_rl/rl.py
@@ -25,39 +25,39 @@ class RandomDecisionPolicy(DecisionPolicy): #Inherit from DecisionPolicy to impl
 
 class QLearningDecisionPolicy(DecisionPolicy):
     def __init__(self, actions, input_dim):
-        self.epsilon = 0.9
-        self.gamma = 0.001
+        self.epsilon = 0.9 #Set the hyper-parameters from the Q-function
+        self.gamma = 0.001 #Set the hyper-parameters from the Q-function
         self.actions = actions
         output_dim = len(actions)
-        h1_dim = 200
+        h1_dim = 200 #Set the number of hidden nodes in the neural networks
 
-        self.x = tf.placeholder(tf.float32, [None, input_dim])
-        self.y = tf.placeholder(tf.float32, [output_dim])
-        W1 = tf.Variable(tf.random_normal([input_dim, h1_dim]))
-        b1 = tf.Variable(tf.constant(0.1, shape=[h1_dim]))
-        h1 = tf.nn.relu(tf.matmul(self.x, W1) + b1)
-        W2 = tf.Variable(tf.random_normal([h1_dim, output_dim]))
-        b2 = tf.Variable(tf.constant(0.1, shape=[output_dim]))
-        self.q = tf.nn.relu(tf.matmul(h1, W2) + b2)
+        self.x = tf.placeholder(tf.float32, [None, input_dim]) #Define the input and output tensors
+        self.y = tf.placeholder(tf.float32, [output_dim]) #Define the input and output tensors
+        W1 = tf.Variable(tf.random_normal([input_dim, h1_dim])) #Design the neural network architecture
+        b1 = tf.Variable(tf.constant(0.1, shape=[h1_dim])) #Design the neural network architecture
+        h1 = tf.nn.relu(tf.matmul(self.x, W1) + b1) #Design the neural network architecture
+        W2 = tf.Variable(tf.random_normal([h1_dim, output_dim])) #Design the neural network architecture
+        b2 = tf.Variable(tf.constant(0.1, shape=[output_dim])) #Design the neural network architecture
+        self.q = tf.nn.relu(tf.matmul(h1, W2) + b2) #Define the op to compute the utility
 
-        loss = tf.square(self.y - self.q)
-        self.train_op = tf.train.AdagradOptimizer(0.01).minimize(loss)
-        self.sess = tf.Session()
-        self.sess.run(tf.initialize_all_variables())
+        loss = tf.square(self.y - self.q) #Set the loss as the square error
+        self.train_op = tf.train.AdagradOptimizer(0.01).minimize(loss) #Use an optimizer to update model parameters to minimize the loss
+        self.sess = tf.Session() #Set up the session and initialize variables
+        self.sess.run(tf.initialize_all_variables()) #Set up the session and initialize variables
 
     def select_action(self, current_state, step):
         threshold = min(self.epsilon, step / 1000.)
-        if random.random() < threshold:
+        if random.random() < threshold: #Exploit best option with probability epsilon
             # Exploit best option with probability epsilon
             action_q_vals = self.sess.run(self.q, feed_dict={self.x: current_state})
             action_idx = np.argmax(action_q_vals)  # TODO: replace w/ tensorflow's argmax
             action = self.actions[action_idx]
-        else:
+        else: # Explore random option with probability 1-epsilon
             # Explore random option with probability 1 - epsilon
             action = self.actions[random.randint(0, len(self.actions) - 1)]
         return action
 
-    def update_q(self, state, action, reward, next_state):
+    def update_q(self, state, action, reward, next_state): #Update the Q-function by updating its model parameters
         action_q_vals = self.sess.run(self.q, feed_dict={self.x: state})
         next_action_q_vals = self.sess.run(self.q, feed_dict={self.x: next_state})
         next_action_idx = np.argmax(next_action_q_vals)
